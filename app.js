20fb163f3603f4831e6f8bed5108b9970728df72
Christopher Gjersing <csgjersing@gmail.com>
2022-04-04 11:49:58 -0700

    Level 4 - bcrypt hash with 10 rounds of salting

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -17,7 +18,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
 
 const User = new mongoose.model('User', userSchema);
 
@@ -34,32 +34,36 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
 
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
   });
 });
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets');
-        }
+        bcrypt.compare(req.body.password, foundUser.password, function(error, result) {
+          if (result) {
+            res.render('secrets');
+          }
+        });
       }
     }
   })
